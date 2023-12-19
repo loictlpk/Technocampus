a5cf7cf5aa61b22f5f48c252a812c0a208191cf4
Loic Tlpk <tulpinckloic@gmail.com>
2023-12-19 15:20:18 +0100

    color ok

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -18,19 +18,19 @@ const HelloButton: React.FC = ({color}) => {
       
       switch (color) {
         case "red":
-          mqttClient.publish(`/groupe4/evt/sif412_L1_in1`, "0");
-          mqttClient.publish(`/groupe4/evt/sif412_L1_in2`, "1");
+          mqttClient.publish(`/groupe4/evt/sif412_L1_in1`, "1");
+          mqttClient.publish(`/groupe4/evt/sif412_L1_in2`, "0");
           mqttClient.publish(`/groupe4/evt/sif412_L1_in3`, "0");
           break;
         case "green":
           mqttClient.publish(`/groupe4/evt/sif412_L1_in1`, "0");
-          mqttClient.publish(`/groupe4/evt/sif412_L1_in2`, "0");
-          mqttClient.publish(`/groupe4/evt/sif412_L1_in3`, "1");
+          mqttClient.publish(`/groupe4/evt/sif412_L1_in2`, "1");
+          mqttClient.publish(`/groupe4/evt/sif412_L1_in3`, "0");
           break;
         case "yellow":
-          mqttClient.publish(`/groupe4/evt/sif412_L1_in1`, "0");
+          mqttClient.publish(`/groupe4/evt/sif412_L1_in1`, "1");
           mqttClient.publish(`/groupe4/evt/sif412_L1_in2`, "1");
-          mqttClient.publish(`/groupe4/evt/sif412_L1_in3`, "1");
+          mqttClient.publish(`/groupe4/evt/sif412_L1_in3`, "0");
           break;
         case "white":
           mqttClient.publish(`/groupe4/evt/sif412_L1_in1`, "1");
